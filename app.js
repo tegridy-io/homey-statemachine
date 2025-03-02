f538b333f4605e56c6dd390a78b4c839b26ece04
Debakel Orakel <debakel.orakel@tegridy.io>
2025-03-02 19:33:54 +0100

    Implement basic driver functionality
    Creates a driver with basic functionality like storing, configuring and
    setting the state.

--- app.js
+++ app.js
@@ -2,13 +2,13 @@
 
 const Homey = require('homey');
 
-module.exports = class MyApp extends Homey.App {
+module.exports = class Statemachine extends Homey.App {
 
   /**
    * onInit is called when the app is initialized.
    */
   async onInit() {
-    this.log('MyApp has been initialized');
+    this.log('Statemachine has been initialized');
   }
 
 };
